de17e944c5801b951f025baaf1a20453bc5d32c1
Ioannis <giannhs95@yahoo.gr>
2022-06-21 17:05:19 +0300

    Add best solution example

---
+++ gecode/int/cost-gcc/example/best-sol-og/cost-gcc-example.hpp
@@ -0,0 +1,45 @@
+#ifndef H_COST_GCC_EXAMPLE
+#define H_COST_GCC_EXAMPLE
+
+#include <gecode/driver.hh>
+#include <gecode/int.hh>
+#include <gecode/minimodel.hh>
+#include "../LI.hpp"
+#include "read-input.hpp"
+
+using namespace Gecode;
+using namespace std;
+
+class CountCostsExample : public Script {
+protected:
+	IntVarArray x;
+	IntVar minCostFlowCost;
+
+public:
+	enum {
+		MODEL_SINGLE, MODEL_MULTI, BRANCHING_SIMPLE, BRANCHING_CUSTOM
+	};
+
+	CountCostsExample(const InstanceOptions& opt);
+	CountCostsExample(CountCostsExample &s) : Script(s) {
+		x.update(*this, s.x);
+		minCostFlowCost.update(*this, s.minCostFlowCost);
+	}
+
+	virtual Space *copy(void) {
+		return new CountCostsExample(*this);
+	}
+
+	void print(ostream& os) const {
+		os << "\tSolution: " << x << "\n" << cost() << endl;
+	}
+
+	void constrain(const Space& _best);
+
+	virtual IntVar cost(void) const {
+		return minCostFlowCost;
+}
+	
+};
+
+#endif
