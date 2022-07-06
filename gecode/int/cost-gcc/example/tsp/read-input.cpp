849540eac0ea86c30315508285dbaa951675750c
Ioannis <giannhs95@yahoo.gr>
2022-07-06 09:34:08 +0300

    Put different TSP input parse methods in functions

--- gecode/int/cost-gcc/example/tsp/read-input.cpp
+++ gecode/int/cost-gcc/example/tsp/read-input.cpp
@@ -18,15 +18,53 @@ double euclidianDistance2D(pair<double, double> pointA,
 	return sqrt(xDiff*xDiff + yDiff*yDiff);
 }
 
-void readInput(string fileName, int& vars, IntSetArgs& domain, IntArgs& vals,
+void read2DEuclidian(ifstream &file, string line, int n, int& vars, IntSetArgs& domain, IntArgs& vals,
+							 IntArgs& lowerBounds, IntArgs& upperBounds, IntArgs& costs) {
+	getline(file, line);
+	getline(file, line);
+
+	vector<pair<double, double>> cord;
+	while (getline(file, line)) {
+		stringstream stream(line);
+		stream >> n;
+		pair<double, double> c(0, 0);
+		stream >> c.first;
+		stream >> c.second;
+		cord.push_back(c);
+	}
+
+	for (int i = 0; i < vars; i++) {
+		for (int j = 0; j < vars; j++) {
+			costs << (int) (0.5 + euclidianDistance2D(cord[i], cord[j]));
+		}
+	}
+}
+
+void readFullMatrix(ifstream &file, string line, int n, int& vars, IntSetArgs& domain, IntArgs& vals,
 							 IntArgs& lowerBounds, IntArgs& upperBounds, IntArgs& costs) {
 
+	getline(file, line);
+	getline(file, line);
+	getline(file, line);
+	getline(file, line);
+
+	for (unsigned int row = 0; row < vars; row++){ 
+		getline(file, line);
+		stringstream stream(line);
+		for (unsigned int i = 0; i < vars; i++) {
+			stream >> n;
+			costs << n;
+		}
+	}
+
+}
+void readFromTSP(string fileName, int& vars, IntSetArgs& domain, IntArgs& vals,
+							 IntArgs& lowerBounds, IntArgs& upperBounds, IntArgs& costs) {
 	string line;
   ifstream file(fileName);
 	if (!file.is_open()) {
 		throw "Could not open file";
 	}
-
 	getline(file, line);
 	getline(file, line);
 	getline(file, line);
@@ -54,41 +92,51 @@ void readInput(string fileName, int& vars, IntSetArgs& domain, IntArgs& vals,
 		lowerBounds << 1;
 	}
 
-	/* 2d euclidian */
-	getline(file, line);
-	getline(file, line);
+	read2DEuclidian(file, line, n, vars, domain, vals, lowerBounds, upperBounds, costs);
+}
 
-	vector<pair<double, double>> cord;
-	while (getline(file, line)) {
-		stringstream stream(line);
-		stream >> n;
-		pair<double, double> c(0, 0);
-		stream >> c.first;
-		stream >> c.second;
-		cord.push_back(c);
+
+
+void readFromGecode(string fileName, int& vars, IntSetArgs& domain, IntArgs& vals,
+							 IntArgs& lowerBounds, IntArgs& upperBounds, IntArgs& costs) {
+	string line;
+  ifstream file(fileName);
+	if (!file.is_open()) {
+		throw "Could not open file";
 	}
+
+	getline(file, line);
+	stringstream stream(line);
+	int n;
+	stream >> vars;
 
 	for (int i = 0; i < vars; i++) {
+		vals << i;
+	}
+	for (int i = 0; i < vars; i++) {
+		vector<int> d;
 		for (int j = 0; j < vars; j++) {
-			costs << (int) (0.5 + euclidianDistance2D(cord[i], cord[j]));
+			if (i != j) {
+				d.push_back(j);
+			}
 		}
+		IntSet tmp;
+		IntSetInit<IntArgs>::init(tmp, IntArgs(d));
+		domain << IntSet(tmp);
+		upperBounds << 1;
+		lowerBounds << 1;
 	}
 
-	/* Full matrix */
-	// getline(file, line);
-	// getline(file, line);
-	// getline(file, line);
-	// getline(file, line);
+	getline(file, line);
+	stream  = stringstream(line);
+	for (int i = 0 ; i < vars*vars; i++) {
+		stream >> n;
+		costs << n;
+	}
+}
 
-	// for (unsigned int row = 0; row < vars; row++){ 
-	// 	getline(file, line);
-	// 	stringstream stream(line);
-	// 	for (unsigned int i = 0; i < vars; i++) {
-	// 		stream >> n;
-	// 		costsD.push_back(n);
-	// 		costs << n;
-	// 	}
-	// }
 
-	file.close();
+void readInput(string fileName, int& vars, IntSetArgs& domain, IntArgs& vals,
+							 IntArgs& lowerBounds, IntArgs& upperBounds, IntArgs& costs) {
+	readFromGecode(fileName, vars, domain, vals, lowerBounds, upperBounds, costs);
 }
