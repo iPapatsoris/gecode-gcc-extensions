356daab6da12304a23e81c78c4d4eaf809365749
Ioannis <giannhs95@yahoo.gr>
2022-08-02 15:24:40 +0300

    Remove examples to have only basic one

--- gecode/int/cost-gcc/example/compile.sh
+++ gecode/int/cost-gcc/example/compile.sh
@@ -5,7 +5,7 @@ g++ -I. -fcx-limited-range -fno-signaling-nans -fno-rounding-math -ffinite-math-
 -c -o read-input.o  read-input.cpp -g
 
 g++ -I. -fcx-limited-range -fno-signaling-nans -fno-rounding-math -ffinite-math-only -fno-math-errno -fno-strict-aliasing -O3 -fvisibility=hidden -std=c++11 -pipe -Wno-unknown-pragmas -Wall -Wextra -fPIC -pthread -DNDEBUG   -DQT_NO_DEBUG \
--c -o flow-graph.o  ../../flow-graph.cpp -g
+-c -o flow-graph.o  ../flow-graph.cpp -g
 
 g++ -o cost-gcc cost-gcc-example.o flow-graph.o read-input.o -g -L. -I. -fcx-limited-range -fno-signaling-nans -fno-rounding-math -ffinite-math-only -fno-math-errno -fno-strict-aliasing -O3 -fvisibility=hidden -std=c++11 -pipe -Wno-unknown-pragmas -Wall -Wextra -fPIC -pthread -DNDEBUG  \
 -lgecodedriver -lgecodegist -lgecodesearch -lgecodeminimodel -lgecodeint -lgecodekernel -lgecodesupport -lpthread 
